0104b62751b933ef3ae2c8c5c0cfe10219df97d7
arsmoriendy <nugrahasteven@gmail.com>
2023-09-07 13:51:32 +0700

    feat(TwitchFollowListModule): auth, simple list

--- src/lib/components/modules/TwitchFollowListModule.svelte
+++ src/lib/components/modules/TwitchFollowListModule.svelte
@@ -1,6 +1,102 @@
 <script lang="ts">
-  let classAlias: string = "";
-  export { classAlias as class };
+  let accessToken: undefined | string = localStorage["twitchAccessToken"];
+  let followList: any[] = [];
+
+  const redirectUri = import.meta.env["DEV"]
+    ? import.meta.env["VITE_TWITCH_REDIRECT_URI"]
+    : "https://api.arsmoriendy.com/NuTabV2";
+
+  // check state, get access token
+  (async () => {
+    if (localStorage["twitchState"] !== undefined) {
+      const tokenResponse = await fetch(
+        `${redirectUri}/token?state=` + localStorage["twitchState"]
+      );
+
+      if (tokenResponse.status === 200) {
+        accessToken = (await tokenResponse.json())["accessToken"];
+      }
+      localStorage.removeItem("twitchState");
+    }
+  })();
+
+  function giveAuthorization(redirectUri: string) {
+    const state = String.fromCharCode(
+      ...crypto.getRandomValues(new Uint32Array(30)).map((int) => {
+        int %= 31;
+        return int < 10 ? int + 48 : int + 87;
+      })
+    );
+    localStorage["twitchState"] = state;
+    location.href =
+      `https://id.twitch.tv/oauth2/authorize?client_id=yffbv3l3u4erjnr9q26nl43qjqw6xz&force_verify=true&redirect_uri=${redirectUri}/auth&response_type=code&scope=user%3Aread%3Afollows&state=` +
+      state;
+  }
+
+  async function getFollowList(
+    accessToken: string,
+    userId: string,
+    after: string | undefined = undefined
+  ): Promise<any[]> {
+    const followURL = new URL("https://api.twitch.tv/helix/streams/followed");
+    followURL.searchParams.append("first", "10");
+    followURL.searchParams.append("user_id", userId);
+    if (after !== undefined) followURL.searchParams.append("after", after);
+
+    const followResponse = await fetch(followURL, {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+        "Client-Id": "yffbv3l3u4erjnr9q26nl43qjqw6xz",
+      },
+    });
+
+    // TODO handle errors
+    const followResponseJSON = await followResponse.json();
+
+    const followList = followResponseJSON["data"];
+    after = followResponseJSON["pagination"]["cursor"];
+
+    if (after === undefined) {
+      return followList;
+    } else {
+      return [
+        ...followList,
+        ...(await getFollowList(accessToken, userId, after)),
+      ];
+    }
+  }
+
+  async function getUserId(accessToken: string): Promise<string> {
+    const response = await fetch("https://api.twitch.tv/helix/users", {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+        "Client-Id": "yffbv3l3u4erjnr9q26nl43qjqw6xz",
+      },
+    });
+    const responseJSON = await response.json();
+    return responseJSON["data"][0]["id"];
+  }
+
+  $: {
+    (async () => {
+      if (accessToken !== undefined) {
+        localStorage["twitchAccessToken"] = accessToken;
+        followList = await getFollowList(
+          accessToken,
+          await getUserId(accessToken)
+        );
+      }
+    })();
+  }
 </script>
 
-Twitch
+{#if accessToken === undefined}
+  <button
+    class="btn btn-primary"
+    on:click={() => giveAuthorization(redirectUri)}>Authenticate</button
+  >
+{:else}
+  {#each followList as i}
+    <p>{i["user_name"]}</p>
+  {/each}
+{/if}
